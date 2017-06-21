584a1c7d98434828e9d6a9f4cd48c66d0a6c0a22
Dayana <dayanalara95@gmail.com>
2017-06-21 17:52:46 -0600

    Basic Files

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>ZINVO</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+<header>
+  <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img src="images/logo.png"></a>
+    </div>
+
+    
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+     
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Men</a></li>
+        <li><a href="#">Women</a></li>
+        <li><a href="#">Straps</a></li>
+        <li><a href="#">Blog</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</header>
+</div>
+
+
+
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include('header.php'); ?>
+<div class="container">
+  
+<div class="banner">
+    <div class="header-inner">
+        <h1>Match Your style to <br> your ambitions</h1>
+        <a href="#">Shop Now</a>
+    </div>
+</div>
+
+<div class="grid">
+  <div class="row">
+    <div class="col-md-6" style="padding-right: 0;">
+        <div class="small-grid small1"></div>
+        <div class="small-grid small2"></div>
+    </div>
+    <div class="col-md-6">
+      <div class="big-grid"></div>
+    </div>
+  </div>
+</div>
+
+<div class="social">
+  <p>#MYZINVO</p>
+  <p>Follow our story on social media</p>
+</div>
+</div>
+
+<?php include('footer.php'); ?>
